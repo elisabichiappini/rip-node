ba4a40487714fccdc9197534bb441a02ba33e384
Elisa Chiappini <elisabichiappini@gmail.com>
2025-11-11 16:44:17 +0100

    feat: metodo per creare array di stringhe

--- myscript.js
+++ myscript.js
@@ -1,12 +1,7 @@
-// const testo = process.argv[2];
-// if(!testo) {
-//     console.log('inserisci un argomento');
-//     process.exit(1);
-// }
-//utilizzando il metodo .slice(2) tolgo i primi due argomenti ossia il percorso dell'eseguibile del node e secondo il percorso dello script
-const argomenti = process.argv.slice(2);
-
-argomenti.forEach(element => {
-    console.log(element); //da terminale ad esempio do : node myscript.js elisa ciao sono felice, verranno stampati uno sotto l'altro gli argomenti
+//metodo per creare array di stringhe .split(',');
+const prodotti = process.env.LISTA.split(',');
+prodotti.forEach((element, index) => {
+    console.log(`${index+1}. ${element}`);
 });
+//da terminale LISTA="ciao,sono,elisa,senza,roba,in,lista" node myscript.js
 
