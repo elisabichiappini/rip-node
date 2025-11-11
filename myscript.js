ca403a9caf18acefa6a7278b62a0cd53d071f2eb
Elisa Chiappini <elisabichiappini@gmail.com>
2025-11-11 16:34:08 +0100

    feat: argomenti  slice 2 e stampo con node

--- myscript.js
+++ myscript.js
@@ -1,7 +1,12 @@
-const testo = process.argv[2];
-if(!testo) {
-    console.log('inserisci un argomento');
-    process.exit(1);
-}
+// const testo = process.argv[2];
+// if(!testo) {
+//     console.log('inserisci un argomento');
+//     process.exit(1);
+// }
+//utilizzando il metodo .slice(2) tolgo i primi due argomenti ossia il percorso dell'eseguibile del node e secondo il percorso dello script
+const argomenti = process.argv.slice(2);
 
-console.log(testo.toUpperCase());
+argomenti.forEach(element => {
+    console.log(element); //da terminale ad esempio do : node myscript.js elisa ciao sono felice, verranno stampati uno sotto l'altro gli argomenti
+});
+
